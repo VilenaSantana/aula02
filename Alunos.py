c7acfdc4179a2587b0b36acef9cd89154c2a95ce
unknown <vilenalsantana@gmail.com>
2025-03-27 15:09:27 -0300

    Adicinei o código aluno

---
+++ Alunos.py
@@ -0,0 +1,5 @@
+nota1= float(input("Digite a primeira nota: "))
+nota2= float(input("Digite a segunda nota: "))
+name= input("Digite seu nome: ")
+media= (nota1 + nota2) /2
+print(f"Olá {name}, sua média é {media}.")
